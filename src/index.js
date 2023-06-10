43f4612c8f6a1330b96abfaac7dde3d048be1377
Eugeshakw <eugeshakw@gmail.com>
2023-06-10 16:34:21 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+// const KEY_API = '37175835-76686085e99d052827f14fa60'
+// const BASE_URL = 'https://pixabay.com/api/'
+// const refs = {
+//     form: document.querySelector('#search-form'),
+//     input: document.querySelector('input[type="text"]'),
+//     btn: document.querySelector('button[type="submit"]'),
+// }
+
+// // const options = {
+// //     headers: {
+// //         authorization: '37175835-76686085e99d052827f14fa60'
+// //     }
+// // }
+
+
+
+
+// fetch(`https://pixabay.com/api/?key=${KEY_API}&&per_page=5&q=yellow&image_type=photo`)
+// .then(r => r.json())
+// .then(console.log)
+
---
+++ src/js/search.js
@@ -0,0 +1,22 @@
+const refs = {
+    btnOne: document.querySelector('.search'),
+    btnTwo: document.querySelector('.custom-button'),
+    inpEl: document.querySelector('.inp-search')
+  };
+  
+ 
+
+ refs.btnTwo.addEventListener('click', (e) => {
+    e.preventDefault();
+ })
+  refs.btnOne.addEventListener('click', (e) => {
+    e.preventDefault();
+    refs.btnTwo.style.display = 'none';
+  })
+
+  refs.inpEl.addEventListener('click', (e) => {
+    e.preventDefault();
+    refs.btnTwo.style.display = 'block';
+  });
+  
+ 
