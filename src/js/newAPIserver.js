bd873e627304b51cce509583de646bccac98a08e
Eugeshakw <eugeshakw@gmail.com>
2023-06-14 15:05:55 +0300

    updateAPI

--- src/js/newAPIserver.js
+++ src/js/newAPIserver.js
@@ -1,8 +1,12 @@
 import axios from "axios"
 
-const KEY_API = '37175835-76686085e99d052827f14fa60'
-const BASE_URL = 'https://pixabay.com/api/'
+
+
+
 export default class imgAPIServer {
+    #KEY_API = '37175835-76686085e99d052827f14fa60'
+    #BASE_URL = 'https://pixabay.com/api/'
+
     constructor() {
         this.page = 1,
         this.searchValue = ''        
@@ -12,9 +16,20 @@ export default class imgAPIServer {
     
     
     async getApi() {
-        const url = `${BASE_URL}/?key=${KEY_API}&per_page=40&q=${this.searchValue}&page=${this.page}`
+        
+        const options ={
+            params: {
+                image_type: 'photo',
+                orientation: 'horizontal',
+                q: this.searchValue,
+                page: this.page,
+                per_page: 40,
+                key: this.#KEY_API,
+                safesearch: true,
+            },
+        }
           
-        const res = await axios.get(url);
+        const res = await axios.get(this.#BASE_URL, options);
         
         this.incrementPage();
         return res.data;      
