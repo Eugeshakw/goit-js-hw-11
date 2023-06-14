916105d7a7495f99033a9f5ac9ed88ecf280a319
Eugeshakw <eugeshakw@gmail.com>
2023-06-14 21:09:25 +0300

    update

--- src/index.js
+++ src/index.js
@@ -129,7 +129,7 @@ async function generateMarkUp() {
                 const { height: cardHeight } = document
                 .querySelector(".gallery")
                 .firstElementChild.getBoundingClientRect();
-
+                
                 window.scrollBy({
                 top: cardHeight * 2,
                 behavior: "smooth",
